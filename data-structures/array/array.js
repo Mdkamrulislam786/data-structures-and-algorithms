ce3ee754b52f59288cbc246fe6ecece546332a1c
MdKamrulislam786 <kamrulislam.ki01@gmail.com>
2021-04-22 14:39:17 +0600

    arrays

---
+++ data-structures/array/array.js
@@ -0,0 +1,60 @@
+//1.Reverse a sstring using vanilla js  ES6/5
+function reverseString(str) {
+  // Step 1. Create an empty string that will host the new created string
+  var newString = "";
+
+  // Step 2. Create the FOR loop
+  /* The starting point of the loop will be (str.length - 1) which corresponds to the 
+       last character of the string, "o"
+       As long as i is greater than or equals 0, the loop will go on
+       We decrement i after each iteration */
+  for (var i = str.length - 1; i >= 0; i--) {
+    newString += str[i]; // or newString = newString + str[i];
+  }
+  /* Here hello's length equals 5
+        For each iteration: i = str.length - 1 and newString = newString + str[i]
+        First iteration:    i = 5 - 1 = 4,         newString = "" + "o" = "o"
+        Second iteration:   i = 4 - 1 = 3,         newString = "o" + "l" = "ol"
+        Third iteration:    i = 3 - 1 = 2,         newString = "ol" + "l" = "oll"
+        Fourth iteration:   i = 2 - 1 = 1,         newString = "oll" + "e" = "olle"
+        Fifth iteration:    i = 1 - 1 = 0,         newString = "olle" + "h" = "olleh"
+    End of the FOR Loop*/
+
+  // Step 3. Return the reversed string
+  return newString; // "olleh"
+}
+
+reverseString("hello");
+
+//1. Witihout comments ES6/5
+function reverseString(str) {
+  var newString = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+}
+reverseString("hello");
+
+//ES6
+const reverseString = (str) => str.split("").reverse().join("");
+reverseString("hello");
+
+//MERGE SORTED ARRAYS
+function sortedArrys(arr1, arr2) {
+  let mergedUnsortedArr = [...arr1, ...arr2];
+  let mergedSortedArr = [];
+  for (let index = 0; index < mergedUnsortedArr.length; index++) {
+    const currElement = mergedUnsortedArr[index];
+    const prevtElement = mergedUnsortedArr[index - 1];
+    console.log(currElement);
+    if (currElement <= !prevtElement) {
+      mergedSortedArr.push(currElement);
+    } 
+  }
+  return mergedSortedArr;
+}
+
+sortedArrys([0, 3, 4, 31], [4, 6, 30]);
+
+[0, 3, 4, 31, 4, 6, 30]
--- data-structures/array/arrays.md
+++ data-structures/array/arrays.md
@@ -2,3 +2,7 @@
 An array is a series of memory locations – or 'boxes' – each of which holds a single item of data, but with each box sharing the same name. 
 Array is a collection of data.
 Array is a typeof object in javascript
+
+### Static vs dynamic array
+In C Static array can store a specific number of elements taht is of the same type. Dynamic array can store as many elements as you want.
+
