6ef7cde7c19e06a502033b9818ad9a442fef9dae
MdKamrulislam786 <kamrulislam.ki01@gmail.com>
2021-04-23 16:40:30 +0600

    hash table

--- data-structures/hash-table/hashtable.js
+++ data-structures/hash-table/hashtable.js
@@ -37,6 +37,17 @@ class HashTable {
     }
     return undefined;
   }
+
+  keys() {
+    const keysArray = [];
+    console.log(this.data.length);
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+    return keysArray;
+  }
 }
 
 const myHashTable = new HashTable(50);
@@ -44,3 +55,51 @@ myHashTable.set('grapes', 10000)
 myHashTable.get('grapes')
 myHashTable.set('apples', 9)
 myHashTable.get('apples')
+myHashTable.keys()
+
+//*******Q2******* */
+
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+
+function firstRecurringCharacter(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if(input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+  return undefined
+}
+
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i]
+    } else {
+      map[input[i]] = i;
+    }
+  }
+  return undefined
+}
+
+firstRecurringCharacter2([1,5,5,1,3,4,6])
+
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
+/* 
+'2': 0
+'5': 1
+'5': 2
+*/
