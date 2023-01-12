d7786a65a0b1bb09d0cc68c6ddabbe762a2c6df2
Kamrul Islam <kamrul.islam@loopfreight.io>
2023-01-12 17:06:00 +0600

    binarySearch+subarray solved

--- Algorithm/Searching/binarySearch.js
+++ Algorithm/Searching/binarySearch.js
@@ -1,20 +1,29 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const N = arr.length;
-function binarySearch(arr, N, target) {
+
+const binarySearch = (arr, N, target) => {
   let left = 0;
   let right = N - 1;
+
   while (left < right) {
     let middleIndex = Math.floor((left + right) / 2);
     if (arr[middleIndex] === target) return middleIndex;
-    if (arr[middleIndex] < target) {
-      left = middleIndex + 1;
-    } else {
-      right = middleIndex - 1;
-    }
+    if (arr[middleIndex] < target) left = middleIndex + 1;
+    else right = middleIndex - 1;
   }
 
-  return right;
-}
+  if (arr[right] === target) {
+    return right;
+  }
+
+  if (arr[right + 1] === target) {
+    return right + 1;
+  }
+
+  return -1;
+};
 
 console.log(binarySearch(arr, N, 6));
 //searching for 9
+
+export { binarySearch };
--- howto-solve-problems/howto-solve-problems.md
+++ howto-solve-problems/howto-solve-problems.md
@@ -16,3 +16,16 @@ If your code has these 2things then your code is good code..
 # howto-solve-problems
 
 Read the cheat sheet to learn the steps to find a good solution
+
+
+# Useful DSA patterns
+ 1. Sliding window
+ 2. Greedy approch
+ 3. 2 pointers
+ 4. Hash map
+ 5. Back tracking - recursion
+ 6. Bit manupulation
+ 7. Binary Search
+ 8. Merge Sort, Quick Sort
+ 9. Graph find union
+ 10. Swaping
