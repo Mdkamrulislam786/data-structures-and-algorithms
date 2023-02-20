9cad0a13a870b17909d05271668db7f9f9d8fccd
Kamrul Islam <kamrul.islam@loopfreight.io>
2023-02-20 13:50:10 +0600

    consequtive sub sequnce

--- problem-sloving/array/findLongestConseqSubseq.js
+++ problem-sloving/array/findLongestConseqSubseq.js
@@ -45,3 +45,43 @@ console.log(
   "Length of the Longest contiguous subsequence is " +
     findLongestConseqSubseq(arr, n)
 );
+
+//SOL 2
+// Javascript program to find longest
+// contiguous subsequence
+
+// Returns length of the longest
+// contiguous subsequence
+function findLongestConseqSubseq2(arr, n) {
+  let S = new Set();
+  let ans = 0;
+
+  // Hash all the array elements
+  for (let i = 0; i < n; i++) S.add(arr[i]);
+
+  // check each possible sequence from
+  // the start then update optimal length
+  for (let i = 0; i < n; i++) {
+    // if current element is the starting
+    // element of a sequence
+    if (!S.has(arr[i] - 1)) {
+      // Then check for next elements
+      // in the sequence
+      let j = arr[i];
+      while (S.has(j)) j++;
+
+      // update optimal length if
+      // this length is more
+      ans = Math.max(ans, j - arr[i]);
+    }
+  }
+  return ans;
+}
+
+// Driver code
+let arr2 = [1, 9, 3, 10, 4, 20, 2];
+let n2 = arr.length;
+console.log(
+  "Length of the Longest contiguous subsequence is " +
+    findLongestConseqSubseq2(arr2, n2)
+);
